6636538290dba5950226571ddfdffe0b409546e5
kimjihooney <111514508+kimjihooney@users.noreply.github.com>
2022-08-31 23:36:30 +0900

    stock_project.c

---
+++ stock_project.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+
+float stock_rise_get (int percent, float stock, int my_money);
+float stock_fall_get (int percent, float stock, int my_money);
+
+int main(void)
+{
+    float QQQ = 300; // 현재 QQQ 주가 : 300달러
+    float stock = 0;
+    int percent = 3; // 오르락 내리락하는 퍼센트
+    float deposit = 300; // 예금도 동일하게 300달러부터 시작
+    float my_stock=0;
+    float my_deposit = 0;
+
+    printf("========================================\n");
+    printf("QQQ 처음 지수 %0.2f\n\n", QQQ);
+
+    //올랐을떄
+    stock = QQQ + QQQ * percent / 100;    
+    my_stock += QQQ;
+    my_deposit += deposit;
+    printf("주식오름 -> 예금 : %0.2f, 주식 : %0.2f\n\n", my_deposit, my_stock);
+
+    //내렸을때
+    // stock = QQQ - QQQ * percent / 100;
+    // stock += QQQ;
+    // my_stock += stock;
+    // my_deposit += deposit; // 예금은 내려도 그대로
+    // printf("주식내림 -> 예금 : %0.2f, 주식 : %0.2f\n\n", my_deposit, my_stock);
+
+
+    //올랐을떄
+    stock = QQQ + QQQ * percent / 100;    
+    my_stock += QQQ;
+    my_deposit += deposit;
+    printf("주식오름 -> 예금 : %0.2f, 주식 : %0.2f\n\n", my_deposit, my_stock);
+    
+    //올랐을떄
+    stock = QQQ + QQQ * percent / 100;    
+    my_stock += QQQ;
+    my_deposit += deposit;
+    printf("주식오름 -> 예금 : %0.2f, 주식 : %0.2f\n\n", my_deposit, my_stock);
+    
+    //올랐을떄
+    stock = QQQ + QQQ * percent / 100;    
+    my_stock += QQQ;
+    my_deposit += deposit;
+    printf("주식오름 -> 예금 : %0.2f, 주식 : %0.2f\n\n", my_deposit, my_stock);
+    
+    //올랐을떄
+    stock = QQQ + QQQ * percent / 100;    
+    my_stock += QQQ;
+    my_deposit += deposit;
+    printf("주식오름 -> 예금 : %0.2f, 주식 : %0.2f\n\n", my_deposit, my_stock);
+    
+    //올랐을떄
+    stock = QQQ + QQQ * percent / 100;    
+    my_stock += QQQ;
+    my_deposit += deposit;
+    printf("주식오름 -> 예금 : %0.2f, 주식 : %0.2f\n\n", my_deposit, my_stock);
+    
+    
+
+    
+
+
+    return 0;
+}
+
+
+
+float stock_rise_get (int percent, float stock, int my_money)
+{
+    float deposit = stock + my_money; // 예금
+
+    stock += stock * percent / 100; // 주식
+    //printf("%d 퍼센트 오른 주식은 %0.2f\n", percent, rise);
+    
+        
+    //printf("총 주식은 %0.2f\n", get);
+    
+    printf("주식오름 -> ");
+    printf("예금 : %0.2f, 주식 : %0.2f\n\n", deposit, stock);
+
+    //printf("QQQ지수 : %0.2f\n", prev_stock * (100 + percent)/ 100);
+
+    return stock;
+}
+
+float stock_fall_get (int percent, float stock, int my_money)
+{
+    float prev_stock = stock - my_money;
+    if (prev_stock == 301.2)
+    {
+        prev_stock = stock;    
+    }
+    else
+    {
+        prev_stock = stock - my_money;
+    }
+
+    stock -= stock * percent / 100;
+    //printf("%d 퍼센트 내린 주식은 %0.2f\n\n", percent, fall);
+    
+
+    
+    float get = stock + my_money;
+    //printf("총 주식은 %0.2f\n", get);
+    printf("QQQ지수 : %0.2f\n", prev_stock * (100 - percent)/ 100);
+    printf("주식내림 -> ");
+    printf("예금 : %0.2f, 주식 : %0.2f\n\n", stock + my_money, get);
+    return get;
+}
