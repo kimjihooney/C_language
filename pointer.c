de53d8438185c39e876016e0a113ae795c78c069
kimjihooney <111514508+kimjihooney@users.noreply.github.com>
2022-09-02 20:58:52 +0900

    pointer.c

--- pointer.c
+++ pointer.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+void swap(int a, int b);
+void swap_addr(int *a, int *b);
+void changeArray(int * ptr);
+
 int main(void)
 {
     // // 포인터
@@ -88,6 +92,54 @@ int main(void)
     // *& 는 아무것도 없는 것과 같다.
     // & 는 주소이며, *는 주소의 값이기 떄문에
 
+    //swap
+    // int a = 10;
+    // int b = 20;
+    // //a와 b의 값을 바꾼다
+
+    // printf("Swap 함수 전 => a : %d, b : %d\n", a, b);
+    // swap(a,b);
+
+    // printf("Swap 함수 후 => a : %d, b : %d\n", a, b);
+
+    // //값에 의한 복사 (Call by Value) -> 값만 복사한다는의미
+
+    // //주소값을 넘기면? 메모리 공간에 있는 주소값 자체를 넘기면..
+    // swap_addr(&a, &b);
+
+    // printf("(addr) Swap 함수 후 => a : %d, b : %d\n", a, b);
+
+    //배열일 떄, arr2 -> 주소
+    int arr2[3] = {10, 20, 30};
+
+    //changeArray(arr2);
+    changeArray(&arr2[0]);
+
+    for (int i =0; i <3; i++)
+    {
+        printf("%d\n", arr2[i]);
+    }
 
     return 0;
+}
+
+void swap(int a, int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("Swap 함수 내 => a : %d, b : %d\n", a, b);
+}
+
+void swap_addr(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    printf("(addr)Swap 함수 내 => a : %d, b : %d\n", *a, *b);
+}
+
+void changeArray(int * ptr)
+{
+    ptr[2] = 50;
 }
