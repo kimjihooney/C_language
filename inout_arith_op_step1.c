d480b14a7ff1b563a9d4a8940b9991f3fd0a93df
kimjihooney <111514508+kimjihooney@users.noreply.github.com>
2022-09-06 16:32:09 +0900

    inout_arith_op_step1.c

---
+++ inout_arith_op_step1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    //step1
+    printf("Hello World\n");
+
+    //step2
+    printf("We love kriii\n");
+
+    //step3
+    int A = 0;
+    int B = 0;
+
+    A = 10;
+    B = 20;
+
+    printf("%d", A+B);
+
+    //step4
+    
+
+
+
+    return 0;
+}
