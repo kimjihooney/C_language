dd07b01663504db198fa8bca689d6ec5585d431d
kimjihooney <111514508+kimjihooney@users.noreply.github.com>
2022-09-09 12:48:02 +0900

    if_bj.c

---
+++ if_bJ.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int A;
+    int B;
+
+    printf("plz input number : ");
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    
+    if(abs(A) <= 10000 && abs(B) <= 10000)
+    {
+        if(A>B)
+        {
+            printf(">");
+        }
+        else if(A<B)
+        {
+            printf("<");
+        }
+        else if(A==B)
+        {
+            printf("==");
+        }
+        else printf("comparison error\n");
+
+    }
+    else printf("input error\n");
+
+
+
+    return 0;
+}
