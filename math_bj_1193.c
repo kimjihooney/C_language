151253fc86cb0a32e95f8d37f7a13bc79c2980c9
kimjihooney <111514508+kimjihooney@users.noreply.github.com>
2022-09-22 22:55:26 +0900

    math_bj_1193.c

---
+++ math_bj_1193.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //math_bj_1193.c
+    int num1 = 1;
+    int num2 = 1;
+
+    int index= 1;
+
+    scanf("%d", &index);
+
+   
+
+    while(1)
+    {
+        if(index % 2) //홀수 인덱스 : 분
+        {
+            num1
+
+        }
+       
+
+
+
+
+    }
+
+
+
+
+    return 0;
+}
